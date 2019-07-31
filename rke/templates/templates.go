3d312686439085e60266c3aeab3b52f139ddd9e3
kinarashah <kinara@rancher.com>
2019-07-31 15:13:14 -0700

    use keys for addon templates

--- rke/templates/templates.go
+++ rke/templates/templates.go
@@ -15,40 +15,83 @@ const (
 	KubeDNS       = "kubeDNS"
 	MetricsServer = "metricsServer"
 	NginxIngress  = "nginxIngress"
+	TemplateKeys  = "templateKeys"
+
+	calicov112 = "calico-v1.12"
+	calicov113 = "calico-v1.13"
+	calicov115 = "calico-v1.15"
+
+	canalv115 = "canal-v1.15"
+	canalv113 = "canal-v1.13"
+	canalv112 = "canal-v1.12"
+
+	flannelv115    = "flannel-v1.15"
+	flannelDefault = "flannel-default"
+
+	coreDnsDefault = "coredns-default"
+	kubeDnsDefault = "kubedns-default"
+
+	metricsServerDefault = "metricsserver-default"
+
+	weaveDefault        = "weave-default"
+	nginxIngressDefault = "nginxingress-default"
 )
 
 func LoadK8sVersionedTemplates() map[string]map[string]string {
 	return map[string]map[string]string{
 		Calico: {
-			"v1.15":   CalicoTemplateV115,
-			"v1.14":   CalicoTemplateV113,
-			"v1.13":   CalicoTemplateV113,
-			"default": CalicoTemplateV112,
+			">=1.15.0":         calicov115,
+			">=1.13.0 <1.15.0": calicov113,
+			"default":          calicov112,
 		},
 		Canal: {
-			"v1.15":   CanalTemplateV115,
-			"v1.14":   CanalTemplateV113,
-			"v1.13":   CanalTemplateV113,
-			"default": CanalTemplateV112,
+			">=1.15.0":         canalv115,
+			">=1.13.0 <1.15.0": canalv113,
+			"default":          canalv112,
 		},
 		Flannel: {
-			"v1.15":   FlannelTemplateV115,
-			"default": FlannelTemplate,
+			">=1.15.0": flannelv115,
+			"default":  flannelDefault,
 		},
 		CoreDNS: {
-			"default": CoreDNSTemplate,
+			"default": coreDnsDefault,
 		},
 		KubeDNS: {
-			"default": KubeDNSTemplate,
+			"default": kubeDnsDefault,
 		},
 		MetricsServer: {
-			"default": MetricsServerTemplate,
+			"default": metricsServerDefault,
 		},
 		Weave: {
-			"default": WeaveTemplate,
+			"default": weaveDefault,
 		},
 		NginxIngress: {
-			"default": NginxIngressTemplate,
+			"default": nginxIngressDefault,
 		},
+		TemplateKeys: getTemplates(),
+	}
+}
+
+func getTemplates() map[string]string {
+	return map[string]string{
+		calicov113: CalicoTemplateV113,
+		calicov115: CalicoTemplateV115,
+		calicov112: CalicoTemplateV112,
+
+		flannelv115:    FlannelTemplateV115,
+		flannelDefault: FlannelTemplate,
+
+		canalv113: CanalTemplateV113,
+		canalv112: CanalTemplateV112,
+		canalv115: CanalTemplateV115,
+
+		coreDnsDefault: CoreDNSTemplate,
+		kubeDnsDefault: KubeDNSTemplate,
+
+		metricsServerDefault: MetricsServerTemplate,
+
+		weaveDefault: WeaveTemplate,
+
+		nginxIngressDefault: NginxIngressTemplate,
 	}
 }
