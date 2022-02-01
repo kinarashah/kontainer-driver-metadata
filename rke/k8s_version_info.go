2e3bfd565ec9c553f12be495c6c299bc699fb9e8
Vaishnav Gaikwad <vaishnavgaikwad721@gmail.com>
2022-02-01 14:14:28 +0530

    go generate

--- rke/k8s_version_info.go
+++ rke/k8s_version_info.go
@@ -593,7 +593,7 @@ func loadK8sVersionInfo() map[string]v3.K8sVersionInfo {
 		"v1.22.5-rancher2-1": {
 			MinRancherVersion: "2.6.3-patch0",
 			MinRKEVersion:     "1.3.3-rc0",
-		}
+		},
 		"v1.22.6-rancher1-1": {
 			MinRancherVersion: "2.6.3-patch0",
 			MinRKEVersion:     "1.3.3-rc0",
