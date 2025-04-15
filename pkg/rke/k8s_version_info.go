ce5aa8438d108ac02dbb5b38836911b79652d414
Kinara Shah <kinara.shah@suse.com>
2025-04-15 13:04:21 -0700

    update min version info

--- pkg/rke/k8s_version_info.go
+++ pkg/rke/k8s_version_info.go
@@ -699,11 +699,11 @@ func loadK8sVersionInfo() map[string]v3.K8sVersionInfo {
 			MinRKEVersion:     "1.6.0-rc0",
 		},
 		"v1.31": {
-			MinRancherVersion: "2.10.0-patch0",
+			MinRancherVersion: "2.10.0-hotfix",
 			MinRKEVersion:     "1.7.0-rc0",
 		},
 		"v1.32": {
-			MinRancherVersion: "2.11.0-patch0",
+			MinRancherVersion: "2.11.0-hotfix",
 			MinRKEVersion:     "1.8.0-rc0",
 		},
 		"v1.22.4-rancher1-1": {
